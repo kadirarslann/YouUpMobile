70bc9d66dafe3be511dc6a821862e90211d420d0
kadirarslann <52932311+kadirarslann@users.noreply.github.com>
2022-03-05 15:47:34 +0300

    reading section rougly done

--- src/components/ReadingItem.js
+++ src/components/ReadingItem.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React,{  useState} from 'react'
 import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,
     TextInput,Image,PreviewLayout} from 'react-native';
 import { Icon } from 'react-native-elements';
-function ReadingItem() {
+function ReadingItem(props) {
+  const [textActive, setTextActive] = useState(false)
+
+  const selectText = (e)=>{
+    setTextActive(!textActive)
+  }
+
+  let tt='aaaaa11aaaa'
   return (
-    <View style={{color:'black',display:'flex',justifyContent:'flex-start'}}>
-      
+    <ScrollView style={{color:'black',display:'flex',flexDirection:'column',width:'100%',padding:10}}>
+      <View  style={{color:'black',display:'flex',flexDirection:'row',width:'100%',alignItems:'center'}}>
         <Icon       
                     name='book'
                     type={'font-awesome'}
-                    color='#f50'
+                    color='green'
                     size={30}
-                    color='red'
-                    backgroundColor='aqua'
+                    onPress={selectText}
                 />
-    </View>
+        <Text style={{color:'black',width:'100%',fontSize:25,paddingLeft:10}}>
+          {props.item.title}
+        </Text>
+        
+      </View>
+      {textActive? <Text style={{color:'black',width:'100%',fontSize:12,overflow:'scroll',paddingBottom:200}}>
+          {props.item.text}
+      </Text> : null}
+      
+    </ScrollView>
+    
   )
 }
 
--- src/components/Readings.js
+++ src/components/Readings.js
@@ -2,18 +2,23 @@ import React from 'react'
 import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,
   TextInput,Image,PreviewLayout} from 'react-native';
 import ReadingItem  from './ReadingItem'
+import readingData from '../static/readings/readingData';
+
 function readings() {
+  
   return (
-    <View style={styles.container}>
-       <Text style={styles.main}>
-      Readings
-      </Text>
+    <View style={styles.container}>  
+      {/* <ReadingItem/>
       <ReadingItem/>
-
-      <ReadingItem/>
-      <ReadingItem/>
-
-      <ReadingItem/>
+      <ReadingItem/> */}
+      {readingData.map((data) => (
+        <ReadingItem key={data.id} item={data}/>
+      ))}
+      {/* <Text style={{color:'red'}}>
+      { readingData[0].text }
+      </Text> */}
+      
+      
     </View>
    
   )
@@ -26,6 +31,11 @@ const styles= StyleSheet.create({
   container:{
     display:'flex',
     padding:0,
+    margin:0
+  },
+  item:{
+    flexGrow:10,
+    width:'100%',
     margin:0
   },
 });
